c059e960b0a5f9fa3c3deee1dcbaa993ca9eeefd
Gugo888 <kroyangurgen@gmail.com>
2022-07-10 12:07:33 +0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,82 +10,81 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { StakingComponent } from './staking/staking.component';
 import { TradingComponent } from './trading/trading.component';
 
+// const routes: Routes = [
+//   {
+//     path:'',
+//     component:CoreComponent,
+//     children: [
+//       {path:'', redirectTo:'home', pathMatch:'full'},
+//       {path:'home', component:HomeComponent},
+//       {path:'market',component:MarketComponent},
+//       {path:'trading',component:TradingComponent},
+//       {path:'news',component:NewsComponent},
+//       {path:'staking',component:StakingComponent},
+//       {path:'games',component:GamesComponent},
+//     ],
+//   },
+    
+//   {
+//     path:'login',
+//     component:LoginComponent
+//   },
+
+//   {
+//     path:'**',
+//     component:NotFoundComponent
+//   }
+// ];
+
 const routes: Routes = [
-  
-  //   path:'',
-  //   component:CoreComponent,
-  //   children:[
-  //     {path:'', redirectTo:'home', pathMatch:'full'},
-  //     {path:'home', component:HomeComponent},
-  //     {path:'market',component:MarketComponent},
-  //     {path:'trading',component:TradingComponent},
-  //     {path:'news',component:NewsComponent},
-  //     {path:'staking',component:StakingComponent},
-  //     {path:'games',component:GamesComponent},
-      
-  //   ]
-  // },
-  {path:'', component:HomeComponent},
-  {path:'market',component:MarketComponent},
-  {path:'trading',component:TradingComponent},
-  {path:'news',component:NewsComponent},
-  {path:'staking',component:StakingComponent},
-  {path:'games',component:GamesComponent},
   {
-    path:'login',
-    component:LoginComponent
+    path: '',
+    component: HomeComponent,
+  },
+
+  // {
+  //   path:'login',
+  //   loadChildren: () => import('../app/registration/login/login.module').then(m => m.LoginModule)
+  // },
+
+  {
+    path: 'market',
+    component: MarketComponent,
   },
 
   {
-    path:'**',
-    component:NotFoundComponent
+    path: 'trading',
+    component: TradingComponent
+  },
+
+  {
+    path: 'news',
+    component: NewsComponent,
+  },
+
+  {
+    path: 'staking',
+    component: StakingComponent,
+  },
+
+  {
+    path: 'games',
+    component: GamesComponent,
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
   }
 ];
 
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: HomeComponent,
-//   },
-
-//   {
-//     path:'login',
-//     loadChildren: () => import('../app/registration/login/login.module').then(m => m.LoginModule)
-//   },
-
-//   {
-//     path: 'market',
-//     component: MarketComponent,
-//   },
-
-//   {
-//     path: 'trading',
-//     component: TradingComponent
-//   },
-
-//   {
-//     path: 'news',
-//     component: NewsComponent,
-//   },
-
-//   {
-//     path: 'staking',
-//     component: StakingComponent,
-//   },
-
-//   {
-//     path: 'games',
-//     component: GamesComponent,
-//   },
-
-//   {
-//     path: '**',
-//     component: NotFoundComponent,
-//   }
-// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
